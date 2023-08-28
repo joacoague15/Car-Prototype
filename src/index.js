dae7e85abd8aeb4f951c207836f1b315a8e9af07
joacoague15 <joacoague15@gmail.com>
2023-08-28 07:33:06 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Howl } from 'howler';
+import Hammer from 'hammerjs';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import openCarSound from './sounds/car-open.wav';
+import startEngineSound from './sounds/start-engine.wav';
+import carMovingSound from './sounds/car-moving.wav';
+import firstCopilotLetIndicationSound from './sounds/first-copilot-left-indication.mp3';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+document.getElementById('playButton').addEventListener('click', () => {
+    const openSound = new Howl({
+        src: [openCarSound],
+        onend: () => startEngine()
+    });
+
+    openSound.play();
+    document.getElementById('playButton').style.display = 'none';
+});
+
+const startEngine = () => {
+    const engineSound = new Howl({
+        src: [startEngineSound],
+        onend: () => carMoving()
+    });
+
+    engineSound.play();
+}
+
+const carMoving = () => {
+    const movingSound = new Howl({
+        src: [carMovingSound],
+        loop: true,
+    });
+
+    copilotStartTalking();
+
+    movingSound.play();
+}
+
+const copilotStartTalking = () => {
+    const firstCopilotIndication = new Howl({
+        src: [firstCopilotLetIndicationSound],
+        onend: () => startHandleSteeringWheel()
+    });
+
+    firstCopilotIndication.play();
+}
+
+const startHandleSteeringWheel = () => {
+    const swipeArea = document.getElementById('swipe-area');
+    const hammerManager = new Hammer(swipeArea);
+
+    hammerManager.add(new Hammer.Swipe({ direction: Hammer.DIRECTION_LEFT }));
+
+    hammerManager.on('swipeleft', (e) => {
+        console.log('You swiped left!');
+    });
+}
