c43cc490442061ab0fbf48e211e8be7760afbae7
joacoague15 <joacoague15@gmail.com>
2023-08-29 01:47:04 +0100

    Added new directions and cassette action

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ import Hammer from 'hammerjs';
 import openCarSound from './sounds/car-open.wav';
 import startEngineSound from './sounds/start-engine.wav';
 import carMovingSound from './sounds/car-moving.wav';
-import firstCopilotLetIndicationSound from './sounds/first-copilot-left-indication.mp3';
+
+import firstCopilotLeftIndicationSound from './sounds/first-copilot-left-indication.mp3';
+import firstCopilotRightIndicationSound from './sounds/first-copilot-right-indication.mp3';
+
+import copilotAskForCassette from './sounds/copilot-ask-for-cassette.mp3';
+
+import turnIndicatorSound from './sounds/turn-indicator.mp3';
+
+import music1 from './sounds/music1.m4a';
+
+import insertCassetteSound from './sounds/insert-cassette.wav';
 
 document.getElementById('playButton').addEventListener('click', () => {
     const openSound = new Howl({
@@ -37,12 +47,12 @@ const carMoving = () => {
 }
 
 const copilotStartTalking = () => {
-    const firstCopilotIndication = new Howl({
-        src: [firstCopilotLetIndicationSound],
+    const copilotIndication = new Howl({
+        src: [firstCopilotLeftIndicationSound],
         onend: startHandleSteeringWheel
     });
 
-    firstCopilotIndication.play();
+    copilotIndication.play();
 }
 
 const startHandleSteeringWheel = () => {
@@ -50,7 +60,73 @@ const startHandleSteeringWheel = () => {
     const hammerManager = new Hammer(swipeArea);
 
     hammerManager.on('swipeleft', (e) => {
-        console.log("SWIPE LEFT");
+        const turnIndication = new Howl({
+            src: [turnIndicatorSound],
+        });
+
+        turnIndication.play();
+
         hammerManager.off('swipeleft', e);
+        secondCopilotIndication();
     });
+}
+
+const secondCopilotIndication = () => {
+    const copilotIndication = new Howl({
+        src: [firstCopilotRightIndicationSound],
+        onend: () => {
+            const swipeArea = document.getElementById('swipe-area');
+            const hammerManager = new Hammer(swipeArea);
+
+            hammerManager.on('swiperight', (e) => {
+                const turnIndication = new Howl({
+                    src: [turnIndicatorSound],
+                });
+
+                turnIndication.play();
+
+                hammerManager.off('swiperight', e);
+                copilotAskForCassete();
+            });
+        }
+    });
+
+    copilotIndication.play();
+}
+
+const copilotAskForCassete = () => {
+    const copilotIndication = new Howl({
+        src: [copilotAskForCassette],
+        onend: insertCasseteAction
+    });
+
+    copilotIndication.play();
+}
+
+const insertCasseteAction = () => {
+    const swipeArea = document.getElementById('swipe-area');
+    const hammerManager = new Hammer(swipeArea);
+
+    // Enable vertical swiping
+    hammerManager.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+    hammerManager.on('swipeup', (e) => {
+        console.log("Swipe up")
+        const insertAction = new Howl({
+            src: [insertCassetteSound],
+            onend: () => playMusic()
+        });
+
+        insertAction.play();
+
+        hammerManager.off('swipeup', e);
+    });
+}
+
+const playMusic = () => {
+    const music = new Howl({
+        src: [music1],
+        loop: true,
+    });
+
+    music.play();
 }
